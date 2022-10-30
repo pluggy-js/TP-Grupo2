c11072d220260475c3902bf46a71fd04c7b49ecd
Roquina Brochero <116672262+RoquiBrochero@users.noreply.github.com>
2022-10-30 18:37:28 -0300

    modifico el main
    cree la estructura general del main.

---
+++ TrabajoC/colaestatica.h
@@ -0,0 +1,49 @@
+#ifndef COLAESTATICA_H_INCLUDED
+#define COLAESTATICA_H_INCLUDED
+
+#define CMAX 15
+
+typedef int tDato;
+
+typedef struct tCola
+{
+    int fin;
+    tDato elem[CMAX];
+}tCola;
+
+void cCrear(tCola* c)
+{
+    c->fin = -1;
+}
+
+int cVacia (tCola* c)
+{
+    return c->fin == -1;
+}
+
+int cLlena(tCola* c)
+{
+    return c->fin == CMAX - 1;
+}
+
+void cPoner(tCola* c, tDato dato)
+{
+    c->fin++;
+    c->elem[c->fin]=dato;
+}
+
+void cSacar(tCola* c, tDato* dato)
+{
+    int i;
+    *dato = c->elem[0];
+
+    for (i=0; i<=c->fin-1; i++ )
+    {
+        c->elem[i] = c->elem[i+1];
+    }
+
+    c->fin--;
+}
+
+
+#endif // COLAESTATICA_H_INCLUDED
---
+++ TrabajoC/piladinamica.h
@@ -0,0 +1,49 @@
+#ifndef PILADINAMICA_H_INCLUDED
+#define PILADINAMICA_H_INCLUDED
+
+typedef int tDato;
+
+typedef struct nodo
+{
+    tDato info;
+    struct nodo* sig;
+}Nodo;
+
+typedef Nodo* tPila;
+
+void pCrear(tPila* p)
+{
+    *p = NULL;
+}
+
+int pVacia (tPila* p)
+{
+    return *p == NULL;
+}
+
+int pLlena(tPila* p)
+{
+    return 0;
+}
+
+void pPoner(tPila* p, tDato dato)
+{
+    Nodo* nodo;
+    nodo=(Nodo*)malloc(sizeof(Nodo));
+    nodo->info=dato;
+    nodo->sig = *p;
+    *p = nodo;
+}
+
+void pSacar(tPila* p, tDato* dato)
+{
+    Nodo* aux;
+    *dato = (*p)->info;
+    aux = (*p);
+    *p = (*p)->sig;
+    free(aux);
+}
+
+
+
+#endif // PILADINAMICA_H_INCLUDED
