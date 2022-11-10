2e235ea41a44c080c67a4d10a1d3c2aa55ec4b91
Gaston Lorenzani <98300310+gastu9372@users.noreply.github.com>
2022-11-10 17:08:21 -0300

    Add files via upload
    Todo muy nashe, 15 operaciones de las listas simplemente enlazadas circulares hechas. Compila todo re piola, pero no puedo hacer que el main lo haga pq colapsa XD

--- TrabajoC/Listas.h
+++ TrabajoC/Listas.h
@@ -3,10 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 typedef int Tdato;
-
-int fin;
-
 Tdato x;
+int existe;
 
 typedef struct Nodo{
     Tdato info;
@@ -27,85 +25,214 @@ NodoL* CrearNodo (struct Tlista *l,Tdato x);
 ///ACA EMPIEZA EL CODIGO
 
 NodoL* CrearNodo(struct Tlista *l,Tdato x){
-    NodoL* nodo= Tlista.cab;
+    NodoL* nodo= l->cab;
     nodo = malloc(sizeof(NodoL));
     nodo->info=x;
-    nodo->sig = Tlista.cab;
+    nodo->sig = l->cab;
     return nodo;
 }
 
-int LFin (struct Tlista *l, int fin){
-    if (Tlista.actual == Tlista.cab){
-        fin = (1);
-    }
-    return (fin);
+//1
+int Lvacia (struct Tlista *l){
+    return l->actual == NULL;
 };
 
+//2
+void Lppio (struct Tlista *l){
+    l->actual = l->cab;
+};
+
+//3
+void Linfo (struct Tlista *l, Tdato x){
+    x = l->actual->info;
+};
+
+//4
+void Lsig (struct Tlista *l){
+    l->actual = l->actual->sig;
+};
+
+//5
+int LFin (struct Tlista *l, int fin){
+    return l->actual == l->cab;
+};
+
+//6
+int Lllena (struct Tlista *l){
+    return 0;  //ESTO SE TIENE QUE RE CONTRA CHEQUEAAAAAAAAAAR
+              //NO ENCUENTRO COMO HACER MEMDISPONIBLE < SIZEOF (NUEVO) XD
+
+};
+
+//7
+void Lcrear (struct Tlista *l){
+    l->cab = NULL;
+};
+
+//8
+void Lmodificiar (struct Tlista *l){
+    l->actual->info = x;
+};
+
+//9
 void LInsertarPpio (struct Tlista *l, Tdato x){
     puntero t, nuevo;
     CrearNodo (nuevo, t);
-        if (Tlista.cab == NULL) {
+        if (l->cab == NULL) {
             nuevo->sig = nuevo;
-            Tlista.cab = nuevo;
+            l->cab = nuevo;
         }else{
-            t = Tlista.cab;
-            while (t->sig != Tlista.cab){
+            t = l->cab;
+            while (t->sig != l->cab){
                 t = t->sig;
             }
-            nuevo->sig = Tlista.cab;
+            nuevo->sig = l->cab;
             t->sig = nuevo;
-            Tlista.cab = nuevo;
+            l->cab = nuevo;
         }
 };
-void lBorrarActual (struct Tlista *l){
-    puntero aux, t;
-    if (Tlista.cab = Tlista.actual){
-        if (Tlista.cab->sig = Tlista.actual){
-            t = Tlista.cab;
-            Tlista.cab = NULL;
-            Tlista.actual = NULL;
-            free (t);
-        }else{
-            aux = Tlista.actual;
-            t = Tlista.cab;
-            while (t->sig != Tlista.cab){
-                t = t->sig;
-            }
-           Tlista.actual = Tlista.actual->sig;
-           Tlista.cab = Tlista.actual;
-           t->sig = Tlista.actual;
-           free (aux);
-        }
+
+//10
+void LInsertarFin (struct Tlista *l, Tdato x){
+    puntero t, nuevo;
+    CrearNodo (nuevo, x);
+    if (l->cab == NULL){
+        nuevo->sig = nuevo;
+        l->cab = nuevo;
     }else{
-        aux = Tlista.actual;
-        t= Tlista.cab;
-        while (t->sig != Tlista.actual){
+        t = l->cab;
+        while (t->sig != l->cab){
             t = t->sig;
         }
-        Tlista.actual = Tlista.actual->sig;
-        t->sig = Tlista.actual;
-        free (aux);
+        nuevo->sig = l->cab;
+        t->sig = nuevo;
+        l->cab = nuevo;
+    }
+};
+
+//11
+void LBorrarPpio (struct Tlista *l){
+    puntero aux, t;
+    aux = l->cab;
+    if (l->cab->sig == l->cab){
+        l->cab = NULL;
+    }else{
+        t = l->cab;
+        while (t->sig != l->cab){
+            t = t->sig;
+        }
+        l->cab = l->cab->sig;
+        t->sig = l->cab;
+    }
+};
+
+//12
+void LBorrarFin (struct Tlista *l){
+    puntero aux, t;
+    t = l->cab;
+    if (l->cab->sig == l->cab){
+        l->cab = NULL;
+        free (t);
+    }else{
+        aux = l->cab;
+        while (aux->sig->sig != l->cab){
+            aux = aux->sig;
+        }
+        t = aux->sig;
+        aux->sig = l->cab;
+        free (t);
     }
 }
 
-void LInsertarOrdenado (struct Tlista *l, Tdato x, char ord){
-    puntero aux, nuevo;
-    CrearNodo(nuevo,x);
-    if (Tlista.cab == Tlista.actual){
-        Tlista.cab = nuevo;
-    }else{
-        if ((x > Tlista.cab->info && ord == 'D') || (x < Tlista.cab->info && ord =='A')) {
-            nuevo->sig = Tlista.cab;
-            Tlista.cab = nuevo; ///HAY QUE REVISAR ACA, SI HAY QUE HACER ALGUN CAMBIO O AGREGAR PARA LAS LISTAS CIRCULARES
+//13
+void lBorrarActual (struct Tlista *l){
+    puntero aux, t;
+    if (l->cab = l->actual){
+        if (l->cab->sig = l->actual){
+            t = l->cab;
+            l->cab = NULL;
+            l->actual = NULL;
+            free (t);
         }else{
-            aux = Tlista.cab;
-            while ((aux->sig != Tlista.actual && (x > aux->sig->info && ord == 'A')) || (x < aux->sig->info && ord == 'D')){
-                aux = aux->sig;
+            aux = l->actual;
+            t = l->cab;
+            while (t->sig != l->cab){
+                t = t->sig;
             }
-            nuevo->sig = aux->sig;
-            aux->sig = nuevo;
+           l->actual = l->actual->sig;
+           l->cab = l->actual;
+           t->sig = l->actual;
+           free (aux);
+        }
+    }else{
+        aux = l->actual;
+        t= l->cab;
+        while (t->sig != l->actual){
+            t = t->sig;
+        }
+        l->actual = l->actual->sig;
+        t->sig = l->actual;
+        free (aux);
+    }
+};
+
+//14
+void LInsertarOrdenado (struct Tlista *l, Tdato x, char ord){
+    puntero t, nuevo;
+    CrearNodo(nuevo,x);
+    if (l->cab == NULL){
+        nuevo->sig = nuevo;
+        l->cab = nuevo;
+    }else{
+        if ((x > l->cab->info && ord == 'D') || (x < l->cab->info && ord =='A')) {
+            t = l->cab;
+            while (t->sig != l->cab){
+                t = t->sig;
+            }
+            nuevo->sig = l->cab;
+            t->sig = nuevo;
+            l->cab = nuevo;
+        }else{
+            t = l->cab;
+            while (t->sig != l->cab && ((x < l->cab->info && ord == 'D') || (x > l->cab->info && ord =='A'))){
+                t = t->sig;
+            }
+            nuevo->sig = t->sig;
+            t->sig = nuevo;
         }
     }
 };
 
+//15
+void LBuscarOrdenado (struct Tlista *l, Tdato x, int existe){
+    puntero t;
+    t = l->cab;
+    existe = 0;
+    if (l->cab != NULL){
+        if (l->cab->info == x){
+            existe = 1;
+            l->actual = l->cab;
+        }else{
+            t = l->cab->sig;
+            while ((t != l->cab) && (t->info == l->cab->info)){
+                t = t->sig;
+            }
+            if (t != l->cab){
+                if (t->info < l->cab->info){
+                    while ((t != l->cab)&&(t->info > x)){
+                        t = t->sig;
+                    }
+                }else{
+                    while ((t != l->cab)&&(t->info < x)){
+                        t = t->sig;
+                    }
+                }
+            }
+            if ((t != l->cab) && (t->info == x)){
+                existe = 1;
+                l->actual = t;
+            }
+        }
+    }
+}
 #endif // LISTAS_H_INCLUDED
--- TrabajoC/main.c
+++ TrabajoC/main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "listas.h"
+Tdato x;
+struct Tlista *l;
 
 int main(){
     printf("Hello world!\n");
     return 0;
-
 };
 
